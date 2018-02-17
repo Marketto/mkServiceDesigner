d9367628be39e725b7f8983e9d4277f1c456ac1e
Marco <marco.ricupero@gmail.com>
2018-02-17 16:25:35 +0100

    SdItem Template migration: + SdItemObject + SdItem (List Mode)

--- src/app/model-editor/sd-item/sd-item-object/sd-item-object.component.ts
+++ src/app/model-editor/sd-item/sd-item-object/sd-item-object.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sd-item-object',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sd-item-object.component.less']
 })
 export class SdItemObjectComponent implements OnInit {
-
+  @Input() item: Object = {};
   constructor() { }
 
   ngOnInit() {
