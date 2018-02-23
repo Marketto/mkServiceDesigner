d0706936f52abcd6f4eabde58ea036df336752f1
Marco <marco.ricupero@gmail.com>
2018-02-23 22:30:34 +0100

    Draft template, style and logic for services treeView

--- src/app/service-tree/service-tree.component.ts
+++ src/app/service-tree/service-tree.component.ts
@@ -1,13 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable, ViewChild } from '@angular/core';
+import { TreeviewItem, TreeviewComponent, TreeviewConfig } from 'ngx-treeview';
 
 @Component({
   selector: 'app-service-tree',
   templateUrl: './service-tree.component.html',
-  styleUrls: ['./service-tree.component.less']
+  styleUrls: ['./service-tree.component.less'],
+  providers: [
+  ]
 })
 export class ServiceTreeComponent implements OnInit {
+  @ViewChild(TreeviewComponent) treeviewComponent: TreeviewComponent;
+  services = [new TreeviewItem({
+    text: '/',
+    value: 0,
+    children : []
+  })];
 
-  constructor() { }
+  treeviewCfg = TreeviewConfig.create({
+    hasAllCheckBox: false,
+    hasCollapseExpand: true,
+    hasFilter: false
+  });
+
+  editMode: Boolean = false;
+  selectedItem: TreeviewItem = null;
+
+  addItem() {
+    const newEndPoint: TreeviewItem = new TreeviewItem({ text: 'new', value: 1});
+    this.selectedItem.children = (this.selectedItem.children || []).concat(newEndPoint);
+  }
+  selectItem(item: TreeviewItem) {
+    this.editMode = this.editMode && this.selectedItem === item;
+    this.selectedItem = item;
+  }
+  editItem(itemElement, item: TreeviewItem) {
+    this.editMode = true;
+    this.selectedItem = item;
+    itemElement.focus();
+  }
+  blurItem() {
+    this.editMode = false;
+    this.selectedItem = null;
+  }
+  keyUpItem(event, item: TreeviewItem) {
+    if (event.keyCode === 13) {
+      this.editMode = false;
+      this.selectedItem = item;
+    }
+  }
+
+  constructor() {}
 
   ngOnInit() {
   }
